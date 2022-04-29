d271024b92263959f8cb906b09a54157cd698c5b
JonasBraun97 <jonas.braun1997@gmx.de>
2022-04-29 17:37:34 -0500

    edit the var autoencoder and incl train function

--- autoencoder/cvae.py
+++ autoencoder/cvae.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Flatten, Dense, InputLayer, Conv2DTranspose, BatchNormalization, Activation
+from tensorflow.keras.callbacks import TerminateOnNaN, CSVLogger, ModelCheckpoint, EarlyStopping
 
 class CVAE(tf.keras.Model):
     """n-dim variational Autoencoder for latent phenotype capture"""
     def __init__(self, args):
         self.inputDir = args.inputDir
         self.outputDir = args.outputDir
+        self.saveDir = args.saveDir
 
         #callback inputs
         self.cvaecbUsage = args.cvaecbUsage
@@ -62,25 +64,12 @@ class CVAE(tf.keras.Model):
                 ]
             )
 
-            # generate latent vector Q(z|X)
-            self.latent = tf.keras.Sequential(
-                [
-                    z2_mean = Dense(self.latent_dim, name='z_mean')(x2)
-            z2_log_var = Dense(self.latent_dim, name='z_log_var')(x2)
-
-            # use reparameterization trick to push the sampling out as input
-            z2 = Lambda(self.sampling, output_shape=(self.latent_dim,), name='z2')([z2_mean, z2_log_var])
-
-            z12=Multiply()([z1,z2])
-                ]
-            )
-
 
             self.decoder = tf.keras.Sequential(
                 [
                     InputLayer(input_shape=(latent_dim,)),
-                    Dense(units=125*125*32, activation='relu'),
-                    Reshape(target_shape=(125, 125, 32))
+                    Dense(units=shape[1]*shape[2]*shape[3], activation='relu'),
+                    Reshape(target_shape=(shape[1], shape[2], shape[3]))
                 ]
             )
 
@@ -126,7 +115,6 @@ class CVAE(tf.keras.Model):
             mean, logvar = tf.split(self.encoder(x), num_or_size_splits=2, axis=1)
             return mean, logvar
 
-        def
 
         def reparametrize(self, sample_args):
             #put this maybe into self
@@ -158,6 +146,95 @@ class CVAE(tf.keras.Model):
             logpz = log_normal_pdf(z, 0., 0.)
             logqz_x = log_normal_pdf(z, mean, logvar)
             return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+        #save model architectures
+        self.model_dir = os.path.join(self.save_dir, 'models')
+        os.makedirs(self.model_dir, exist_ok=True)
+        print('saving model architectures to', self.model_dir)
+        with open(os.path.join(self.model_dir, 'arch_vae.json'), 'w') as file:
+            file.write(self.vae.to_json())
+        with open(os.path.join(self.model_dir, 'arch_encoder.json'), 'w') as file:
+            file.write(self.encoder.to_json())
+        with open(os.path.join(self.model_dir, 'arch_decoder.json'), 'w') as file:
+            file.write(self.decoder.to_json())
+
+    def train(self):
+        """ train VAE model
+        """
+        #Note: Datagenerator including on the fly rotation, orientation, and size transformations not included here. Implement according to needs and use to load images.
+
+        print('Loading Input1 Images')
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+            self.inputDir,
+            label_mode= None,
+            validation_split = 0.2,
+            subset = "training",
+            seed = 123,
+            image_size = (self.imageSize, self.imageSize),
+            batch_size=self.batchSize,)
+
+        callbacks = []
+        #terminates when the loss is NaN
+        term_nan = TerminateOnNaN()
+        callbacks.append(term_nan)
+
+        #save callbacks in a csv file
+        csv_logger = CSVLogger(os.path.join(self.save_dir, 'training.log'),
+                           separator='\t')
+        callbacks.append(csv_logger)
+
+        checkpointer = ModelCheckpoint(
+            os.path.join(self.save_dir, 'checkpoints/vae_weights.hdf5'),
+            verbose=1,
+            save_best_only=True,
+            save_weights_only=True)
+        callbacks.append(checkpointer)
+
+        if self.earlystop:
+            earlystop = EarlyStopping(monitor='loss', min_delta=0, patience=8)
+            callbacks.append(earlystop)
+
+
+        #if self.use_vaecb:
+            #vaecb = VAEcallback(self)
+            #callbacks.append(vaecb)
+
+        self.history = self.vae.fit(
+            x={"encoder_input1": self.input1_data, "encoder_input2": self.input2_data},
+            y=out1_data,
+            epochs = self.epochs,
+            callbacks = callbacks,
+            batch_size=self.batch_size)
+
+
+        # imageList=sorted(glob.glob(os.path.join(self.data_dir,'train', '*')))
+
+        #data = []
+        #for imagePath in imageList:
+            #image = imread(imagePath)
+            #image=resize(image,(self.image_size, self.image_size, self.nchannel))
+            #image=image*(255/np.max(image))
+            #data.append(image)
+        #self.input1_data = np.array(data, dtype="float") / 255.0
+
+
+        #print('Loading Output Images')
+        #imageList=sorted(glob.glob(os.path.join(out1_dir,'train', '*')))
+        #data = []
+        #for imagePath in imageList:
+            #image = imread(imagePath)
+            #image=resize(image,(self.image_size, self.image_size, self.nchannel))
+            #image=image*(255/np.max(image))
+            #data.append(image)
+        #out1_data = np.array(data, dtype="float") / 255.0
+
+        print('saving model weights to', self.model_dir)
+        self.vae.save_weights(os.path.join(self.model_dir, 'weights_vae.hdf5'))
+        self.encoder.save_weights(os.path.join(self.model_dir, 'weights_encoder.hdf5'))
+        self.decoder.save_weights(os.path.join(self.model_dir, 'weights_decoder.hdf5'))
+        self.encode()
+
+        print('done!')
 
 
 
