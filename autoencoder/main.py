7bd5a42cc2c7a1c7f0c3bc27e3e086a3a866dab4
JonasBraun97 <jonas.braun1997@gmx.de>
2022-05-05 16:09:48 -0500

    included load weights

--- autoencoder/main.py
+++ autoencoder/main.py
@@ -8,9 +8,11 @@ import os
 from tensorboard import program
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--inputDir',       type=str,   default='/Users/jones/Library/CloudStorage/OneDrive-NorthwesternUniversity/ownProject/data/mergedDAPIYFPNormalized_Subregion_13_r2_c3/singleCellImages',     help='input data directory (in train subfolder)')
+parser.add_argument('--earlystop',		type=str,	default='train',			help='train or load model')
+parser.add_argument('--inputDir',       type=str,   default='/Users/jbj7388/Library/CloudStorage/OneDrive-NorthwesternUniversity/ownProject/data/mergedDAPIYFPNormalized_Subregion_13_r2_c3/singleCellImages',     help='input data directory (in train subfolder)')
 #parser.add_argument('--outputDir',       type=str,   default='OutputImages/',     help='output 2 data directory (in train subfolder)')
 #parser.add_argument('--saveDir',       type=str,   default='Outputs/',     help='save directory')
+parser.add_argument('--weights',       type=str,   default='Outputs/checkpoints',     help='model weight directory')
 
 parser.add_argument('--earlystop',		type=int,	default=1,			help='use early stopping? 1=yes, 0=no')
 
@@ -41,73 +43,80 @@ learnRateList = [0.0001, 0.001]
 filtersList = [16, 32]
 
 def main():
-    runNumber = 0
-    for i in range(len(layersList)):
-        for j in range(len(latentList)):
-            for k in range(len(kernelList)):
-                args.nlayers = layersList[i]
-                args.latentDim = latentList[j]
-                args.kernelSize = kernelList[k]
-                foldername = 'Outputs' + str(runNumber)
-                os.mkdir(foldername)
-                otherFolder = foldername + '/checkpoints'
-                os.mkdir(otherFolder)
-                args.saveDir = foldername + '/'
-                print('Created new Directory to store Output')
-                #macbookPathImage = '/Users/jones/Library/CloudStorage/OneDrive-NorthwesternUniversity/ownProject/data/mergedDAPIYFPNormalized_Subregion_13_r2_c3/singleCellImages'
-                print('Reading in images')
-                train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-                    args.inputDir,
-                    label_mode= None,
-                    seed = 123,
-                    image_size = (500, 500),
-                    batch_size=args.batchSize,)
-                print('Reading in successful')
-                resize_and_rescale = tf.keras.Sequential([
-                    tf.keras.layers.experimental.preprocessing.Resizing(args.imageSize,args.imageSize),
-                    tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-                ])
-                aug_ds = train_ds.map(
-                    lambda x : (resize_and_rescale(x)))
-                print('images transformed')
+    print('Reading in images')
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        args.inputDir,
+        label_mode= None,
+        seed = 123,
+        image_size = (500, 500),
+        batch_size=args.batchSize,)
+    print('Reading in successful')
+    resize_and_rescale = tf.keras.Sequential([
+        tf.keras.layers.experimental.preprocessing.Resizing(args.imageSize,args.imageSize),
+        tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+    ])
+    aug_ds = train_ds.map(
+        lambda x : (resize_and_rescale(x)))
+    print('images transformed')
 
-                print('Create callbacks')
-                callbacks = []
-                term_nan = TerminateOnNaN()
-                callbacks.append(term_nan)
-                csv_logger = CSVLogger(join(args.saveDir, 'training.log'), separator='\t')
-                callbacks.append(csv_logger)
-                checkpoint = ModelCheckpoint(join(args.saveDir, 'checkpoints/cvae_weights.hdf5'), verbose = 1, save_best_only=True, save_weights_only=True, monitor='loss')
-                callbacks.append(checkpoint)
-                tb_name = 'CVAE_nlayers' + str(args.nlayers) + '_lr' + str(args.learnRate) + '_latent' + str(args.latentDim) + '_filters' + str(args.nfilters) + '_kernel' + str(args.kernelSize) + '_{}'.format(int(time.time()))
-                tb = TensorBoard(log_dir= join(args.saveDir, tb_name), histogram_freq=1, embeddings_freq = 2)
-                callbacks.append(tb)
+    if args.mode == 'train':
+        runNumber = 0
+        for i in range(len(layersList)):
+            for j in range(len(latentList)):
+                for k in range(len(kernelList)):
+                    args.nlayers = layersList[i]
+                    args.latentDim = latentList[j]
+                    args.kernelSize = kernelList[k]
+                    foldername = 'Outputs' + str(runNumber)
+                    os.mkdir(foldername)
+                    otherFolder = foldername + '/checkpoints'
+                    os.mkdir(otherFolder)
+                    args.saveDir = foldername + '/'
+                    print('Created new Directory to store Output')
+                    print('Create callbacks')
+                    callbacks = []
+                    term_nan = TerminateOnNaN()
+                    callbacks.append(term_nan)
+                    csv_logger = CSVLogger(join(args.saveDir, 'training.log'), separator='\t')
+                    callbacks.append(csv_logger)
+                    checkpoint = ModelCheckpoint(join(args.saveDir, 'checkpoints/cvae_weights.hdf5'), verbose = 1, save_best_only=True, save_weights_only=True, monitor='loss')
+                    callbacks.append(checkpoint)
+                    tb_name = 'CVAE_nlayers' + str(args.nlayers) + '_lr' + str(args.learnRate) + '_latent' + str(args.latentDim) + '_filters' + str(args.nfilters) + '_kernel' + str(args.kernelSize) + '_{}'.format(int(time.time()))
+                    tb = TensorBoard(log_dir= join(args.saveDir, tb_name), histogram_freq=1, embeddings_freq = 2)
+                    callbacks.append(tb)
 
-                if args.earlystop:
-                    earlystop = EarlyStopping(monitor='loss', min_delta=0, patience=8)
-                    callbacks.append(earlystop)
+                    if args.earlystop:
+                        earlystop = EarlyStopping(monitor='loss', min_delta=0, patience=8)
+                        callbacks.append(earlystop)
 
-                optimizer = tf.keras.optimizers.Adam(args.learnRate)
-                # set the dimensionality of the latent space to a plane for visualization later
-                num_examples_to_generate = 16
-                random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate, args.latentDim])
-                print('Init model')
-                model = CVAE(args)
-                print('Compile model')
-                model.compile(optimizer = optimizer)
-                print('Model compiled. model.fit starts')
-                model.fit(aug_ds, epochs = args.epochs, batch_size=args.batchSize, callbacks=callbacks)
+                    optimizer = tf.keras.optimizers.Adam(args.learnRate)
+                    # set the dimensionality of the latent space to a plane for visualization later
+                    num_examples_to_generate = 16
+                    random_vector_for_generation = tf.random.normal(shape=[num_examples_to_generate, args.latentDim])
+                    print('Init model')
+                    model = CVAE(args)
+                    print('Compile model')
+                    model.compile(optimizer = optimizer)
+                    print('Model compiled. model.fit starts')
+                    model.fit(aug_ds, epochs = args.epochs, batch_size=args.batchSize, callbacks=callbacks)
 
 
-                modelDir = os.path.join(args.saveDir, 'models')
-                os.makedirs(modelDir, exist_ok=True)
-                print('Save model weights to', modelDir)
-                model.save_weights(join(modelDir, 'weights_CVAE.hdf5'))
+                    modelDir = os.path.join(args.saveDir, 'models')
+                    os.makedirs(modelDir, exist_ok=True)
+                    print('Save model weights to', modelDir)
+                    model.save_weights(join(modelDir, 'weights_CVAE.hdf5'))
 
-                print('Model saved. Start encoding')
-                model.encode(aug_ds)
-                print('DONE!!')
-                runNumber += 1
+                    print('Model saved. Start encoding')
+                    model.encode(aug_ds)
+                    print('DONE!!')
+                    runNumber += 1
+
+    if args.mode == 'load':
+        if args.checkpoint == 'NA':
+            sys.exit('No weight file provided')
+        model = CVAE(args)
+        model.cvae.load_weights(args.weights)
+        model.encode(aug_ds)
 
 
 if __name__ == '__main__':
